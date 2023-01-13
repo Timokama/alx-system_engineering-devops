e56c457712c70602fa3647f5c9c099ab6107e966
Timothy Kamau <kamautimothy635@gmail.com>
2023-01-13 08:34:09 +0000

    Install a package.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,13 +4,13 @@ exec { 'apt-get update':
   command => '/usr/bin/apt-get update'
 }
 
-package { 'rubygems1.9.1':
-  ensure => 'installed',
+package { 'pip3':
+  ensure  => 'installed',
   require => Exec['apt-get update']
 }
 
-package {'puppet-lint':
-  ensure => '2.1.1'
-  require => Package['rubygems1.9.1'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  require  => Package['pip3'],
   provider => 'gem'
 }
